55425ea653597f30d10051b7ebddc8eb2a9147f7
Anthony <146886278+TonyM-Analyst@users.noreply.github.com>
2026-04-30 17:14:22 -0400

    solution JS

--- twosum.js
+++ twosum.js
@@ -5,5 +5,15 @@
 // */
 
 var twoSum = function(nums, target) {
-    
+    const map = {};
+
+    for (let i = 0; i < nums.length; i++) {
+        const complement = target - nums[i];
+
+        if (map.hasOwnProperty(complement)) {
+            return [map[complement], i];
+        }
+
+        map[nums[i]] = i;
+    }
 };
